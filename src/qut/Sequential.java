62ce549a0fb8facfccb2ef5d9f0f31224d6aabe5
Thien <tuthien.tran@connect.qut.edu.au>
2024-10-10 10:32:08 +1000

    debug: Sequential.java and referenceGenes.list

--- src/qut/Sequential.java
+++ src/qut/Sequential.java
@@ -98,24 +98,21 @@ public class Sequential
     public static void run(String referenceFile, String dir) throws FileNotFoundException, IOException
     {             
         List<Gene> referenceGenes = ParseReferenceGenes(referenceFile);
-        for (String filename : ListGenbankFiles(dir))
-        {
+        for (String filename : ListGenbankFiles(dir)) {
             System.out.println(filename);
             GenbankRecord record = Parse(filename);
-            for (Gene referenceGene : referenceGenes)
-            {
-		System.out.println(referenceGene.name);
-                for (Gene gene : record.genes)
-                    if (Homologous(gene.sequence, referenceGene.sequence))
-                    {
+            for (Gene referenceGene : referenceGenes) {
+		        System.out.println(referenceGene.name);
+                for (Gene gene : record.genes) {
+                    if (Homologous(gene.sequence, referenceGene.sequence)) {
                         NucleotideSequence upStreamRegion = GetUpstreamRegion(record.nucleotides, gene);
-	                Match prediction = PredictPromoter(upStreamRegion);
-                        if (prediction != null)
-                        {
+                        Match prediction = PredictPromoter(upStreamRegion);
+                        if (prediction != null) {
                             consensus.get(referenceGene.name).addMatch(prediction);
                             consensus.get("all").addMatch(prediction);
                         }
                     }
+                }
             }
         }
 
